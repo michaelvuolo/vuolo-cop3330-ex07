4e9e6bb3d88ede5ba39641d4d5a56d2a764cb9b0
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 16:59:25 -0400

    init

---
+++ src/main/java/edu/ucf/exercise.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+package edu.ucf;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String[] args) {
+//        // prompt for age
+//        System.out.print("What is your current age? ");
+//
+//        // create new scanner
+//        Scanner input = new Scanner(System.in);
+//
+//        // read age
+//        int age = Integer.parseInt(input.nextLine());
+//
+//        // prompt for retirement age
+//        System.out.print("At what age would you like to retire? ");
+//
+//        // read retirement age
+//        int retirementAge = Integer.parseInt(input.nextLine());
+//
+//        // calculate years left
+//        int yearsleft = retirementAge - age;
+//
+//        // output years left
+//        System.out.print("You have " + yearsleft + " years left until you can retire." + "\n");
+//
+//        // get current year
+//        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+//
+//        // calculate retirement year
+//        int retirementYear = currentYear + yearsleft;
+//        System.out.print( "It's " + currentYear + " so you can retire in " + retirementYear + ".");
+
+        
+    }
+}
