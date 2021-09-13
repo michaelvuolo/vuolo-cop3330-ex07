446de703f4979536494735b86ceadfa33ca69daf
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 17:19:01 -0400

    Update exercise.java

--- src/main/java/edu/ucf/exercise.java
+++ src/main/java/edu/ucf/exercise.java
@@ -9,34 +9,6 @@ import java.util.Scanner;
 
 public class exercise {
     public static void main(String[] args) {
-//        // prompt for age
-//        System.out.print("What is your current age? ");
-//
-//        // create new scanner
-//        Scanner input = new Scanner(System.in);
-//
-//        // read age
-//        int age = Integer.parseInt(input.nextLine());
-//
-//        // prompt for retirement age
-//        System.out.print("At what age would you like to retire? ");
-//
-//        // read retirement age
-//        int retirementAge = Integer.parseInt(input.nextLine());
-//
-//        // calculate years left
-//        int yearsleft = retirementAge - age;
-//
-//        // output years left
-//        System.out.print("You have " + yearsleft + " years left until you can retire." + "\n");
-//
-//        // get current year
-//        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-//
-//        // calculate retirement year
-//        int retirementYear = currentYear + yearsleft;
-//        System.out.print( "It's " + currentYear + " so you can retire in " + retirementYear + ".");
-
         // prompt for room length
         System.out.print("What is the length of the room in feet? ");
 
